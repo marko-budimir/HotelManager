b12315ec1ecd488cd812d62142b7908c50a7cba9
Lsasko <luka.sasko123@gmail.com>
2024-02-21 15:59:05 +0100

    ﻿PagedList start

--- HotelManager/HotelManager.Repository/HotelServiceRepository.cs
+++ HotelManager/HotelManager.Repository/HotelServiceRepository.cs
@@ -263,10 +263,14 @@ namespace HotelManager.Repository
             {
                 NpgsqlCommand command = new NpgsqlCommand();
 <<<<<<< HEAD
+<<<<<<< HEAD
                 command.CommandText = "SELECT COUNT(\"Id\") FROM \"Service\"";
 =======
                 command.CommandText = "SELECT COUNT(\"Id\") FROM \"Discount\"";
 >>>>>>> 0ef429c (PagedList start)
+=======
+                command.CommandText = "SELECT COUNT(\"Id\") FROM \"Discount\"";
+>>>>>>> 20e4b0d (PagedList start)
                 ApplyFilter(command, hotelServiceFilter);
                 command.Connection = connection;
                 try
