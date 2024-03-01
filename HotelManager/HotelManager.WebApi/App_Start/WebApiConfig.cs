f1bc8586df478d4833c7dca8d340135ba78c8606
Lsasko <luka.sasko123@gmail.com>
2024-03-01 09:44:01 +0100

    Cors and json for frontend

--- HotelManager/HotelManager.WebApi/App_Start/WebApiConfig.cs
+++ HotelManager/HotelManager.WebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace HotelManager.WebApi
 {
@@ -10,7 +11,11 @@ namespace HotelManager.WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            var cors = new EnableCorsAttribute(
+             origins: "https://localhost:3000",
+             headers: "*",
+             methods: "*");
+            config.EnableCors(cors);
             // Web API routes
             config.MapHttpAttributeRoutes();
 
