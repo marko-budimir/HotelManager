cb50a8ff282037e1746d470e9044f0a91fe21ded
DESKTOP-Q5279HB\lukah <luka.hat58@gmail.com>
2024-03-05 16:52:07 +0100

    add logic for dashboard services

--- HotelMenager-Frontend/src/components/services/ServiceAddForm.js
+++ HotelMenager-Frontend/src/components/services/ServiceAddForm.js
@@ -1,27 +1,66 @@
 import React from "react";
+import { useState } from "react";
+import { createService } from "../../services/api_hotel_service";
+import { useNavigate } from "react-router-dom";
 
 export const ServiceAddForm = () => {
+  const navigate = useNavigate();
+  const [service, setServiceData] = useState({
+    name: "",
+    description: "",
+    price: 0,
+  });
+
+  const handleChange = (e) => {
+    setServiceData({
+      ...service,
+      [e.target.name]: e.target.value,
+    });
+
+    console.log({
+      ...service,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await createService(service);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="service-add">
       <h2 className="service-add-heading">Add Service</h2>
-      <form className="service-add-form">
+      <form className="service-add-form" onSubmit={handleSubmit}>
         <label className="service-add-form-label">Name:</label>
         <input
           className="service-add-form-input"
           placeholder="Enter service name"
           type="text"
+          name="name"
+          onChange={handleChange}
         />
         <label className="service-add-form-label">Description:</label>
         <textarea
           className="service-add-form-input"
           placeholder="Enter service description"
+          onChange={handleChange}
+          name="description"
         />
         <label className="service-add-form-label">Price:</label>
         <input
           className="service-add-form-input"
           placeholder="Enter service price"
           type="number"
+          onChange={handleChange}
+          name="price"
         />
+        <input type="submit" value="Add" />
       </form>
     </div>
   );
