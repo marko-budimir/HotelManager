9a710ddae0f2d4d8c04aa3ce0e04b50c60a30cb3
to0ony <antonijomaric1@gmail.com>
2024-03-07 15:28:40 +0100

    DashboardNavigation conflict fix

--- HotelManager-Frontend/src/components/navigation/DashboardNavigation.js
+++ HotelManager-Frontend/src/components/navigation/DashboardNavigation.js
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 const DashboardNavigation = () => {
   return (
-    <div className="dashboard-navigation">   
-        <h2>Dashboard</h2>
-        <nav className="navigation">
+    <div className="dashboard-navigation">
+      <h2>Dashboard</h2>
+      <nav className="navigation">
         <Link to="/dashboardRoom">Rooms</Link>
         <Link to="/dashboardReceipt">Receipts</Link>
         <Link to="/dashboard-reservation">Reservations</Link>
         <Link to="/dashboardServices">Services</Link>
         <Link to="/dashboard-discount">Discounts</Link>
         <Link to="/dashboard-roomtype">Room types</Link>
-        </nav>
+      </nav>
     </div>
   );
 };
