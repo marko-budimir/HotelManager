ff50f6fd4a982bd5088d407c174a34333e802209
to0ony <antonijomaric1@gmail.com>
2024-03-07 13:53:06 +0100

    Minor changes

--- HotelManager-Frontend/src/components/navigation/DashboardNavigation.js
+++ HotelManager-Frontend/src/components/navigation/DashboardNavigation.js
@@ -2,14 +2,17 @@ import { Link } from "react-router-dom";
 
 const DashboardNavigation = () => {
   return (
-    <nav>
-      <Link to="/dashboardRoom">Rooms</Link>
-      <Link to="/dashboardReceipt">Receipts</Link>
-      <Link to="/dashboard-reservation">Reservations</Link>
-      <Link to="/dashboardServices">Services</Link>
-      <Link to="/dashboard-discount">Discounts</Link>
-      <Link to="/dashboard-roomtype">Room types</Link>
-    </nav>
+    <div className="dashboard-navigation">   
+        <h2>Dashboard</h2>
+        <nav className="navigation">
+        <Link to="/dashboardRoom">Rooms</Link>
+        <Link to="/dashboardReceipt">Receipts</Link>
+        <Link to="/dashboard-reservation">Reservations</Link>
+        <Link to="/dashboardServices">Services</Link>
+        <Link to="/dashboard-discount">Discounts</Link>
+        <Link to="/dashboard-roomtype">Room types</Link>
+        </nav>
+    </div>
   );
 };
 
